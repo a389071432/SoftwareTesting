3541a9864fbeb1aedace14c76d8650ece472f9ec
T20210430 <529914423@qq.com>
2021-04-26 19:52:08 +0800

    Triangle前端

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,64 +1,63 @@
 <template>
-    <div class="header">
-        <!-- 折叠按钮 -->
-        <div class="collapse-btn" @click="collapseChage">
-            <i v-if="!collapse" class="el-icon-s-fold"></i>
-            <i v-else class="el-icon-s-unfold"></i>
-        </div>
-        <div class="logo">软件测试练习</div>
-        <div class="header-right">
-        </div>
+  <div class="header">
+    <!-- 折叠按钮 -->
+    <div class="collapse-btn" @click="collapseChage">
+      <i v-if="!collapse" class="el-icon-s-fold"></i>
+      <i v-else class="el-icon-s-unfold"></i>
     </div>
+    <div class="logo">软件测试练习</div>
+    <div class="header-right"></div>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            fullscreen: false,
-            name: "linxin",
-            message: 2
-        };
-    },
-    computed: {
-        collapse() {
-            return this.$store.state.collapse;
-        }
-    },
-    methods: {
-        // 侧边栏折叠
-        collapseChage() {
-            this.$store.commit("hadndleCollapse", !this.collapse);
-        }
-    },
-    mounted() {
-        if (document.body.clientWidth < 1500) {
-            this.collapseChage();
-        }
+  data() {
+    return {
+      fullscreen: false,
+      name: "linxin",
+      message: 2
+    };
+  },
+  computed: {
+    collapse() {
+      return this.$store.state.collapse;
     }
+  },
+  methods: {
+    // 侧边栏折叠
+    collapseChage() {
+      this.$store.commit("hadndleCollapse", !this.collapse);
+    }
+  },
+  mounted() {
+    if (document.body.clientWidth < 1500) {
+      this.collapseChage();
+    }
+  }
 };
 </script>
 <style scoped>
 .header {
-    position: relative;
-    box-sizing: border-box;
-    width: 100%;
-    height: 70px;
-    font-size: 22px;
-    color: #fff;
+  position: relative;
+  box-sizing: border-box;
+  width: 100%;
+  height: 70px;
+  font-size: 22px;
+  color: #fff;
 }
 .collapse-btn {
-    float: left;
-    padding: 0 21px;
-    cursor: pointer;
-    line-height: 70px;
+  float: left;
+  padding: 0 21px;
+  cursor: pointer;
+  line-height: 70px;
 }
 .header .logo {
-    float: left;
-    width: 250px;
-    line-height: 70px;
+  float: left;
+  width: 250px;
+  line-height: 70px;
 }
 .header-right {
-    float: right;
-    padding-right: 50px;
+  float: right;
+  padding-right: 50px;
 }
 </style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,65 +2,70 @@ import {createRouter, createWebHistory} from "vue-router";
 import Home from "../views/Home.vue";
 
 const routes = [
-    {
-        path: '/',
-        redirect: '/dashboard'
-    }, {
-        path: "/",
-        name: "Home",
-        component: Home,
-        children: [
-            {
-                path: "/dashboard",
-                name: "dashboard",
-                meta: {
-                    title: '系统首页'
-                },
-                component: () => import ("../views/Dashboard.vue")
-            }, {
-                path: "/table",
-                name: "basetable",
-                meta: {
-                    title: '表格'
-                },
-                component: () => import ("../views/BaseTable.vue")
-            }, {
-                path: "/form",
-                name: "baseform",
-                meta: {
-                    title: '表单'
-                },
-                component: () => import ("../views/BaseForm.vue")
-            }
-        ]
-    }, {
-        path: "/login",
-        name: "Login",
+  {
+    path: '/',
+    redirect: '/dashboard'
+  }, {
+    path: "/",
+    name: "Home",
+    component: Home,
+    children: [
+      {
+        path: "/dashboard",
+        name: "dashboard",
         meta: {
-            title: '登录'
+          title: '系统首页'
         },
-        component: () => import ("../views/Login.vue")
-    }
+        component: () => import ("../views/Dashboard.vue")
+      },  {
+        path: "/triangle",
+        name: "triangle",
+        meta: {
+          title: '三角形'
+        },
+        component: () => import ("../views/Triangle.vue")
+      }, {
+        path: "/table",
+        name: "basetable",
+        meta: {
+          title: '表格'
+        },
+        component: () => import ("../views/BaseTable.vue")
+      }, {
+        path: "/form",
+        name: "baseform",
+        meta: {
+          title: '表单'
+        },
+        component: () => import ("../views/BaseForm.vue")
+      }
+    ]
+  }, {
+    path: "/login",
+    name: "Login",
+    meta: {
+      title: '登录'
+    },
+    component: () => import ("../views/Login.vue")
+  }
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 });
 
 router.beforeEach((to, from, next) => {
-    document.title = `${to.meta.title} | vue-manage-system`;
-    const role = localStorage.getItem('ms_username');
-    if (!role && to.path !== '/login') {
-        next('/login');
-    } else if (to.meta.permission) {
-        // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
-        role === 'admin'
-            ? next()
-            : next('/403');
-    } else {
-        next();
-    }
+  document.title = `${to.meta.title} | vue-manage-system`;
+  const role = localStorage.getItem('ms_username');
+  if (!role && to.path !== '/login') {
+    next('/login');
+  } else if (to.meta.permission) {
+    // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
+    role === 'admin'? next(): next('/403');
+  } else {
+    next();
+  }
 });
 
 export default router;
